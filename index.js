8acb057707c52642cba5afd1e02953e2b67ff95f
plasmacrack <100508195+plasmacrack@users.noreply.github.com>
2022-02-27 16:10:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-screen.orientation.lock("portrait");
-
 var now = moment().format("D MMM, h.mA");
 document.getElementById("date").innerHTML = now;
 
 var platform = new H.service.Platform({
   'apikey': 'zNt7PkvIDBA-4CiFc4mX0wa3GIIQYN_5KVH4tROc5uY'
 });
+
 
 var geocoder = platform.getSearchService();
 if(navigator.geolocation) {
@@ -24,3 +23,11 @@ if(navigator.geolocation) {
 } else {
     console.error("Geolocation is not supported by this browser!");
 }
+
+
+if (document.querySelector(".bg").requestFullscreen)
+  document.querySelector(".bg").requestFullscreen();
+else if (document.querySelector(".bg").webkitRequestFullScreen)
+  document.querySelector(".bg").webkitRequestFullScreen();
+
+screen.orientation.lock("portrait");
